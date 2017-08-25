ec05e5f0772ed553fef1a31eccab7a72370294a1
Michał <rykunek@hotmail.com>
2017-08-25 22:53:24 +0200

    Add Presentation Model

---
+++ app/models/Presentation.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PresentationSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  instructor: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  subscription: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  accessKey: {
+    type: String,
+    minlength: 4,
+    maxlength: 4,
+    unique: true,
+    required: true,
+  },
+  options: {
+    viewersLimit: {
+      type: Number,
+      required: true,
+    },
+    pollsLimit: {
+      type: Number,
+      required: true,
+    },
+    questionsEnabled: {
+      type: Boolean,
+      required: true,
+    },
+    questionsModerated: {
+      type: Boolean,
+      required: true,
+    },
+    slidesEnabled: {
+      type: Boolean,
+      required: true,
+    },
+    slidesLimit: {
+      type: Number,
+      required: true,
+    }
+  },
+
+});
+
+const Presentation = mongoose.model('Presentation', PresentationSchema);
+
+module.exports = { Presentation };
